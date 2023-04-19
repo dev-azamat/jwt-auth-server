8c90b635c29519f901dd63434d3d590429e983ce
azamat <azamatkhabibullaevv@gmail.com>
2023-04-19 13:39:46 -0700

    Add user controllers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const Router = require('express').Router
+const { registration, login, logout, activate, refresh, getUsers } = require('../controllers/userController')
 
 const router = new Router()
 
-router.post('/registration')
-router.post('/login')
-router.post('/logout')
-router.get('/activate/:link')
-router.get('/refresh')
-router.get('/users') // test endpoint
+router.post('/registration', registration)
+router.post('/login', login)
+router.post('/logout', logout)
+router.get('/activate/:link', activate)
+router.get('/refresh', refresh)
+router.get('/users', getUsers) // test endpoint
 
 module.exports = router
