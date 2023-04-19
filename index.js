d2558d77632cc02bcf8c22b957f55bb47954f4e4
azamat <azamatkhabibullaevv@gmail.com>
2023-04-19 09:23:51 -0700

    Add middlewares

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URI, {
