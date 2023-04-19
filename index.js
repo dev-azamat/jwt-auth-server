7a598a274974eb0ccc827e62a38757fd25da9c91
azamat <azamatkhabibullaevv@gmail.com>
2023-04-19 09:22:59 -0700

    Connect MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
@@ -9,6 +10,10 @@ const app = express()
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
   } catch (e) {
     console.log(e)
