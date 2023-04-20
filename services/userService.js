1feb5a59ab8f968e529c6051a0beee8f607b1443
azamat <azamatkhabibullaevv@gmail.com>
2023-04-20 12:27:05 -0700

    Implement JSON Web Token

--- services/userService.js
+++ services/userService.js
@@ -2,6 +2,8 @@ const UserModel = require('../models/userModel')
 const bcrypt = require('bcrypt')
 const uuid = require('uuid')
 const mailService = require('./mailService')
+const tokenService = require('./tokenService')
+const UserDto = require('../dtos/userDto')
 
 class UserService {
   async registration(email, password) {
@@ -13,6 +15,14 @@ class UserService {
     const activationLink = uuid.v4()
     const user = await UserModel.create({email, password: hashPassword, activationLink})
     await mailService.sendActivationMail(email, activationLink)
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({...userDto})
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return {
+      ...tokens,
+      user: userDto
+    }
   }
 }
 
